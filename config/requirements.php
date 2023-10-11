c59046ec5c2c9165e781a1751d6fa4229415d209
Kevin From <kevinfrom@live.dk>
2023-10-11 18:16:22 +0200

    format error strings

--- config/requirements.php
+++ config/requirements.php
@@ -20,25 +20,25 @@ foreach ($requiredExtensions as $requiredExtension) {
     }
 }
 
-$requiredMemoryLimitInBytes = 256 * 1024 * 1024;
+$requiredMemoryLimitInMb = 256;
 $memoryLimit = parseByteStringAsBytes(ini_get('memory_limit'));
 
-if ($memoryLimit < $requiredMemoryLimitInBytes) {
-    trigger_error('Minimum PHP \"memory_limit\" of 256MB required.', E_USER_ERROR);
+if ($memoryLimit < ($requiredMemoryLimitInMb * 1024 * 1024)) {
+    trigger_error("'Minimum PHP \"memory_limit\" of {$requiredMemoryLimitInMb}MB required.", E_USER_ERROR);
 }
 
-$requiredMaxPostInBytes = 128 * 1024 * 1024;
+$requiredMaxPostInMb = 128;
 $postMaxSize = parseByteStringAsBytes(ini_get('post_max_size'));
 
-if ($postMaxSize < $requiredMaxPostInBytes) {
-    trigger_error('Minimum PHP "post_max_size" limit of 128MB required.', E_USER_ERROR);
+if ($postMaxSize < ($requiredMaxPostInMb * 1024 * 1024)) {
+    trigger_error("Minimum PHP \"post_max_size\" limit of {$requiredMaxPostInMb}MB required.", E_USER_ERROR);
 }
 
-$requiredUploadMaxFilesizeInBytes = 128 * 1024 * 1024;
+$requiredUploadMaxFilesizeInMb = 128;
 $uploadMaxFilesize = parseByteStringAsBytes(ini_get('upload_max_filesize'));
 
-if ($uploadMaxFilesize < $requiredUploadMaxFilesizeInBytes) {
-    trigger_error('Minimum PHP "upload_max_filesize" of 128MB required.', E_USER_ERROR);
+if ($uploadMaxFilesize < ($requiredUploadMaxFilesizeInMb * 1024 * 1024)) {
+    trigger_error("Minimum PHP \"upload_max_filesize\" of {$requiredUploadMaxFilesizeInMb}MB required.", E_USER_ERROR);
 }
 
 ini_set('display_errors', false);
