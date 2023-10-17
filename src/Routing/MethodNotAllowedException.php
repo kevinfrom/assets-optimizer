10f64c5960235fd6477f4595b7f2746bbd2bd72a
Kevin From <kevinfrom@live.dk>
2023-10-17 13:35:40 +0200

    fixed exception message

--- src/Routing/MethodNotAllowedException.php
+++ src/Routing/MethodNotAllowedException.php
@@ -6,6 +6,6 @@ class MethodNotAllowedException extends BadRequestException
 {
     public function __construct(string $method)
     {
-        parent::__construct("$method allowed.", 400);
+        parent::__construct("$method required.", 400);
     }
 }
